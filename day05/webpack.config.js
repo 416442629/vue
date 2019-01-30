01cd8915eff380b481762af94d1855a17ab3c520
张伟龙 <33744312+416442629@users.noreply.github.com>
2019-01-30 10:35:39 +0800

    Update webpack.config.js

--- day05/webpack.config.js
+++ day05/webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
       filename: 'index.html' // 指定生成的页面的名称
     })
   ],
+  //mode: 'development' ,
   module: { // 这个节点，用于配置 所有 第三方模块 加载器 
     rules: [ // 所有第三方模块的 匹配规则
       { test: /\.css$/, use: ['style-loader', 'css-loader'] }, //  配置处理 .css 文件的第三方loader 规则
